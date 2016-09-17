ee63799acbad358f0d9b7d8a5c25046dabcdf035
Raghu Ugare <raghu.ugare@gmail.com>
2016-09-17 23:20:50 +0530

    Extra detail added.

--- sqrt_newton_raphson.py
+++ sqrt_newton_raphson.py
@@ -1,5 +1,5 @@
 # Finding an approximation to the square-root of a number via the Newton-Raphson method.
-
+# Can be extended to find the n'th root of a number too!
 x = int(raw_input('Approximation of Square-root via the Newton-Raphson method--Enter an integer: '))
 epsilon = 0.01
 guess = x/2.0
